90e7be2638f351a2bbac5d89b52a0e3f35ffed1a
Omar Ashour <omar.a.ashour@gmail.com>
2024-06-26 16:36:25 -0300

    Make Login Responsive to Tablet Size

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,25 +5,27 @@ import Link from "next/link";
 import { icons } from '../../config';
 import TextInputWithIcon from '../../components/UI/TextInputWithIcon';
 import Button from '../../components/UI/Button';
+import LogoSmall from '../../components/logo/LogoSmall';
 
 export default function Page() {
   // to be integrated into logic
   const isError = false;
 
   return (
-    <section className='flex min-h-full flex-1 flex-col justify-center px-8 py-8 space-y-16 lg:px-8'>
-      <header className="flex items-center gap-1.5 sm:w-full">
+    <section className='flex min-h-full flex-1 flex-col justify-center px-8 py-8 space-y-16 md:px-40 md:py-56 md:space-y-12 lg:px-8'>
+      <header className="flex items-center md:justify-center gap-1.5 sm:w-full">
         <figure>
-          <Image src='/logo/logo-devlinks-small.svg' alt='logo' width={32} height={32} />
+          {/* <Image src='/logo/logo-devlinks-small.svg' alt='logo' width={32} height={32} /> */}
+          <LogoSmall width={40} height={40} />
         </figure>
-        <h1 className="text-3xl font-semibold">
+        <h1 className="text-3xl md:text-4xl font-semibold">
           devlinks
         </h1>
       </header>
 
-      <main>
+      <main className="md:p-10">
         <div>
-          <h2 className="mb-2 text-grey-400 text-2xl font-bold">
+          <h2 className="mb-2 text-grey-400 text-2xl md:mb-4 md:text-3xl font-bold">
             Login
           </h2>
           <p className="text-grey-300">
@@ -56,8 +58,8 @@ export default function Page() {
             </div>
           </form>
 
-          <div className="mt-6 text-center">
-            <p className='text-grey-300 mb-0.5 '>
+          <div className="flex flex-col mt-6 text-center justify-center md:gap-1 md:flex-row">
+            <p className='text-grey-300'>
               Don&apos;t have an account?
             </p>
             <Link href="/signup" className="text-purple-300">
